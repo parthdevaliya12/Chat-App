6de8945908322c12e79da2d816cf12b287fe64c6
ParthDevaliya522 <parthdevaliya1@gmail.com>
2026-08-15 17:07:52 +0530

    "Add live url"

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.MODE === 'development' ? '/api' : 'https://chat-app-rdnr.onrender.com/api';
+
 export const axiosInstance = axios.create({
-  baseURL: '/api',
+  baseURL: BASE_URL,
   withCredentials: true,
 });
--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5001' : '/';
+const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5001' : 'https://chat-app-rdnr.onrender.com';
 
 let socket = null;
 

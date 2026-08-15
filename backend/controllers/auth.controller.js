7bf4a6594f206f073552950d0005c3300994d20e
ParthDevaliya522 <parthdevaliya1@gmail.com>
2026-08-15 17:19:09 +0530

    "Some change"

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -10,7 +10,7 @@ const generateToken = (userId, res) => {
   res.cookie('jwt', token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true,
-    sameSite: 'strict',
+    sameSite: process.env.NODE_ENV !== 'development' ? 'none' : 'strict',
     secure: process.env.NODE_ENV !== 'development',
   });
 };
